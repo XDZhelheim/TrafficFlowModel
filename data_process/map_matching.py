3f1876ea6b50982eecf4576a7d78137252d0fca6
XDZhelheim <601251915@qq.com>
2021-11-16 16:00:36 +0800

    update 21.11.16

--- data_process/map_matching.py
+++ data_process/map_matching.py
@@ -25,7 +25,7 @@ def to_numpy_array(shared_array, shape):
     return arr.reshape(shape)
 
 lock=mp.Lock()
-np_mat=np.zeros((len(df_roads), len(df_roads)), dtype=np.int32)
+np_mat=np.zeros((9, len(df_roads), len(df_roads)), dtype=np.int32)
 shared_mat=to_shared_array(np_mat, ctypes.c_int32)
 trans_matrix=to_numpy_array(shared_mat, np_mat.shape)
 
@@ -34,26 +34,32 @@ def get_distance(row, midpoint):
 
     return row
 
+def get_seconds(row):
+    row["time_seconds"]=row["gps_time"].hour*3600+row["gps_time"].minute*60+row["gps_time"].second
+    return row
+
 def resample_coords(df_taxi):
     index_drop_list=[]
-    last_time=df_taxi.iloc[0]["gps_time"]+pd.Timedelta(seconds=-30)
+    last_time=df_taxi.iloc[0]["time_seconds"]-30
     for index, row in df_taxi.iterrows():
-        if row["gps_time"]-last_time<pd.Timedelta(seconds=30):
+        if row["time_seconds"]-last_time<30:
             index_drop_list.append(index)
             continue
-        last_time=row["gps_time"]
+        last_time=row["time_seconds"]
     df_taxi=df_taxi.drop(index_drop_list)
     coords=df_taxi[["lat", "lng"]].values
+    time_list=df_taxi["time_seconds"].values
 
-    return df_taxi, coords
+    return df_taxi, coords, time_list
 
 def get_track(file_path):
     global df_roads
 
     df_taxi=pd.read_csv(file_path)
     df_taxi["gps_time"]=pd.to_datetime(df_taxi["gps_time"])
+    df_taxi=df_taxi.apply(get_seconds, axis=1)
 
-    df_taxi, coords=resample_coords(df_taxi)
+    df_taxi, coords, time_list=resample_coords(df_taxi)
     midpoint=coords[int(len(coords)/2)]
 
     df_roads_temp=df_roads.apply(get_distance, args=(midpoint,), axis=1)
@@ -73,7 +79,8 @@ def get_track(file_path):
                 track.append(road)
                 if last_road!=-1:
                     with lock:
-                        trans_matrix[last_road, road]+=1
+                        trans_matrix[0, last_road, road]+=1
+                        trans_matrix[int(time_list[i]/10800)+1, last_road, road]+=1
                         # print(last_road, road, trans_matrix[last_road, road])
                 find=True
                 last_road=road
@@ -82,20 +89,22 @@ def get_track(file_path):
             track.append(None)
 
     df_taxi["track"]=track
-    df_taxi.to_csv("../data/taxi/tracks/{}".format(file_path.split("/")[-1]), index=False)
+    df_taxi.to_csv("../data/taxi_after_proc/tracks/{}".format(file_path.split("/")[-1]), index=False)
     
-    print("../data/taxi/tracks/{}".format(file_path.split("/")[-1]))
+    print("../data/taxi_after_proc/tracks/{}".format(file_path.split("/")[-1]))
 
 N=30
 
 if __name__ == "__main__":
-    pool=mp.Pool(16)
+    pool=mp.Pool(32)
 
-    for file in os.listdir("../data/taxi/processed/")[:N]:
-        file_path="../data/taxi/processed/"+file
-        res=pool.apply_async(get_track, (file_path,))
+    for file in os.listdir("../data/taxi_after_proc/merged/"):
+        file_path="../data/taxi_after_proc/merged/"+file
+        date=int(file.split("-")[1].split("_")[0])
+        if date>=2 and date<=7:
+            res=pool.apply_async(get_track, (file_path,))
 
     pool.close()
     pool.join()
 
-    np.save("../data/trans_matirx.npy", trans_matrix)
+    np.save("../data/trans_matrix.npy", trans_matrix)
